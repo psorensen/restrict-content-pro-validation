3d72f4ba86e4b2b54d59902047c578651deb7d74
Peter Sorensen <psorensen@openminds.com>
2017-04-07 13:48:21 -0700

    wip

--- assets/js/src/restrict-content-pro-form-validation.js
+++ assets/js/src/restrict-content-pro-form-validation.js
@@ -5,100 +5,68 @@
  * Copyright (c) 2015 Peter Sorensen
  * Licensed under the GPLv2+ license.
  */
- 
-( function( window, undefined ) {
-	'use strict';
 
-	var $ = jQuery;
-	var user_url = rcpfv_script_options.ajaxurl + '?action=om_validate_user';
-	var email_url = rcpfv_script_options.ajaxurl + '?action=om_validate_email';
-	
+(function (window, undefined) {
+    'use strict';
 
-  $( "#rcp_registration_form" ).validate({
-    debug: true,
-    rules: {
-      rcp_user_login: {
-        required: true,
-        remote: {
-          url: user_url,
-          type: "post",
-          data: {
-            rcp_user_email: function() {
-              return $( "#rcp_user_login" ).val();
+    var $ = jQuery;
+    var user_url = rcpfv_script_options.ajaxurl + '?action=om_validate_user';
+    var email_url = rcpfv_script_options.ajaxurl + '?action=om_validate_email';
+    var form = $("#rcp_registration_form");
+
+    form.validate({
+        debug: true,
+        rules: {
+            rcp_user_login: {
+                required: true,
+                remote: {
+                    url: user_url,
+                    type: "post",
+                    data: {
+                        rcp_user_email: function () {
+                            return $("#rcp_user_login").val();
+                        }
+                    }
+                }
+            },
+            rcp_user_email: {
+                required: true,
+                email: true,
+                remote: {
+                    url: email_url,
+                    type: "post",
+                    data: {
+                        rcp_user_email: function () {
+                            return $("#rcp_user_email").val();
+                        }
+                    }
+                }
+            },
+            rcp_user_pass_confirm: {
+                equalTo: '#rcp_password'
             }
-          }
-        }
-      },      
-      rcp_user_email: {
-        required: true,
-        email: true,
-        remote: {
-          url: email_url,
-          type: "post",
-          data: {
-            rcp_user_email: function() {
-              return $( "#rcp_user_email" ).val();
+        },
+        messages: {
+            rcp_user_pass_confirm: {
+                equalTo: "Passwords do not match"
+            },
+            rcp_user_login: {
+                remote: "This username has already been registered"
+            },
+            rcp_user_email: {
+                email: 'Please enter a valid email address',
+                remote: "This email address is already registered. <a href='/wp-login.php''>Log in</a>?"
             }
-          }
+        },
+    });
+
+    $(document).on('click', '#rcp_submit', function(e){
+        if ( ! form.valid() ) {
+            return;
         }
-      },
-      rcp_user_pass_confirm : {
-        equalTo : '#rcp_password'
-      }
-    },
-    messages : {
-      rcp_user_pass_confirm : {
-        equalTo: "Passwords do not match"
-      },
-      rcp_user_login : {
-        remote: "This username has already been registered"
-      },
-      rcp_user_email : {
-        email: 'Please enter a valid email address',
-        remote: "This email address is already registered. <a href='#''>Log in</a>?"
-      }
-    }
-  });
+    });
+
+})(this);
 
 
-
-
-
-  // var config = {
- //    // ordinary validation config
- //    form : 'form',
- //    // reference to elements and the validation rules that should be applied
- //    validate : {
- //      '#rcp_user_login' : {
- //         validation : 'server,required',
- //         url: user_url
- //      },
- //      '#rcp_user_email' : {
- //         validation : 'server,email,required',
- //         url : email_url
- //      },
- //      '#rcp_user_first' : {
- //      	validation : 'required'
- //      }, 
- //      '#rcp_user_last' : {
- //      	validation : 'required'
- //      },      
- //      '#rcp_password' : {
- //      	validation : 'required'
- //      },
- //      '#rcp_password_again' : {
- //      	validation : 'required, confirmation',
- //      	confirm : "#rcp_password"
- //      },
- //    }
- //  };
-
- //  $.validate({
-	//    modules : 'jsconf, security',
-	//    addValidClassOnAll : true,
-	//    onModulesLoaded : function() {
-	//     $.setupValidation( config );
-	//    }
-	// });
-
-} )( this );
+// form.valid()
--- includes/functions/scripts.php
+++ includes/functions/scripts.php
@@ -3,20 +3,23 @@
  * Enqueue scripts and styles
  * TODO: Use Local Version of Validation Script
  */
-function om_rcp_scripts() {
-	// wp_enqueue_script( 'jquery-form-validator', '//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.43/jquery.form-validator.min.js', array('jquery'), '1.0.0', true  );
-	wp_enqueue_script( 'jquery-form-validator', '//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.js', array('jquery'), '1.0.0', true  );
-	wp_enqueue_script( 'restrict-content-pro-form-validation', RCPFV_URL . '/assets/js/restrict-content-pro-form-validation.min.js', array('jquery'), '1.0.0', true );
-	wp_enqueue_style( 'restrict-content-pro-form-validation-style', RCPFV_URL . '/assets/css/restrict-content-pro-form-validation.min.css');
 
-	wp_localize_script( 
-		'restrict-content-pro-form-validation', 
-		'rcpfv_script_options', 
-		array( 
-			'ajaxurl' => admin_url( 'admin-ajax.php' ) 
-		) 
-	);
-}
 
 // Enqueue scripts only when RCP registration form is loaded
-add_action( 'rcp_before_register_form_fields', 'om_rcp_scripts' );
+add_action( 'wp_enqueue_scripts', function () {
+
+//    if ( ! function_exists( 'rcp_is_registration') && ! rcp_is_registration() ) return;
+
+    // wp_enqueue_script( 'jquery-form-validator', '//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.43/jquery.form-validator.min.js', array('jquery'), '1.0.0', true  );
+    wp_enqueue_script( 'jquery-form-validator', '//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.js', array('jquery'), '1.0.0', true  );
+    wp_enqueue_script( 'restrict-content-pro-form-validation', RCPFV_URL . '/assets/js/restrict-content-pro-form-validation.min.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_style( 'restrict-content-pro-form-validation-style', RCPFV_URL . '/assets/css/restrict-content-pro-form-validation.min.css');
+
+    wp_localize_script(
+        'restrict-content-pro-form-validation',
+        'rcpfv_script_options',
+        array(
+            'ajaxurl' => admin_url( 'admin-ajax.php' )
+        )
+    );
+} );
